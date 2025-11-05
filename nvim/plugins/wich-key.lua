90b0f03d0c9a4e51a0802c73f3e89799ec5c3bba
Erik Welander <erik.welander@volvocars.com>
2025-11-05 18:48:41 +0100

    Format-code

--- nvim/plugins/wich-key.lua
+++ nvim/plugins/wich-key.lua
@@ -2,7 +2,7 @@ return { -- Useful plugin to show you pending keybinds.
   "folke/which-key.nvim",
   event = "VimEnter", -- Sets the loading event to 'VimEnter'
   config = function() -- This is the function that runs, AFTER loading
-    local ws = require("which-key");
+    local ws = require("which-key")
     ws.setup()
     -- Document existing key chains
     ws.add({
